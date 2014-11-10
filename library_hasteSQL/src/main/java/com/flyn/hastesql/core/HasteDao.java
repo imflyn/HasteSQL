356d5a55d5a7317c6a01b4ff3cd9f4fa6032ca91
imflyn <imflyn@163.com>
2014-11-10 21:42:18 +0800

    New change

---
+++ library_hasteSQL/src/main/java/com/flyn/hastesql/core/HasteDao.java
@@ -0,0 +1,8 @@
+package com.flyn.hastesql.core;
+
+/**
+ * Created by flyn on 2014-11-10.
+ */
+public class HasteDao
+{
+}
---
+++ library_hasteSQL/src/main/java/com/flyn/hastesql/optional/QueryBuilder.java
@@ -0,0 +1,8 @@
+package com.flyn.hastesql.optional;
+
+/**
+ * Created by flyn on 2014-11-10.
+ */
+public class QueryBuilder
+{
+}
