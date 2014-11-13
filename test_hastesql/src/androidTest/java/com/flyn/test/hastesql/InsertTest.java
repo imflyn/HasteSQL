8b916d1e9336d8359e89fd393929d6424291ab08
imflyn <imflyn@163.com>
2014-11-13 23:06:23 +0800

    new change

---
+++ test_hastesql/src/androidTest/java/com/flyn/test/hastesql/InsertTest.java
@@ -0,0 +1,58 @@
+package com.flyn.test.hastesql;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import com.flyn.hastesql.HasteSQL;
+import com.flyn.hastesql.core.HasteMaster;
+import com.flyn.test.hastesql.entity.People;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by flyn on 2014-11-12.
+ */
+public class InsertTest extends AndroidTestCase
+{
+
+    private Context mContext;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        mContext = getContext();
+        assertNotNull(mContext);
+
+
+    }
+
+    public void testInsert()
+    {
+        HasteMaster hasteMaster = HasteSQL.createDefault(mContext);
+
+        People people = new People();
+        hasteMaster.insert(people);
+
+    }
+
+    public void testInsertAll()
+    {
+        HasteMaster hasteMaster = HasteSQL.createDefault(mContext);
+        People people;
+        List<People> testMultiModelList = new LinkedList<People>();
+        for (int i = 0; i < 10000; i++)
+        {
+            people = new People();
+            testMultiModelList.add(people);
+        }
+        hasteMaster.insertAll(testMultiModelList);
+    }
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+}
---
+++ test_hastesql/src/androidTest/java/com/flyn/test/hastesql/entity/People.java
@@ -0,0 +1,20 @@
+package com.flyn.test.hastesql.entity;
+
+import com.flyn.hastesql.core.HasteModel;
+
+import java.util.Date;
+
+/**
+ * Created by flyn on 2014-11-13.
+ */
+public class People implements HasteModel
+{
+
+    private int id1 = 1;
+
+    private String name = "123";
+
+    private int age = 19;
+
+    private Date date = new Date();
+}
