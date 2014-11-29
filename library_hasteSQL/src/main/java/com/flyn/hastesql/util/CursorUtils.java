d911c37e44e5c13987203df5a8b7515f949e83a7
imflyn <imflyn@163.com>
2014-11-29 18:12:07 +0800

    new change

--- library_hasteSQL/src/main/java/com/flyn/hastesql/util/CursorUtils.java
+++ library_hasteSQL/src/main/java/com/flyn/hastesql/util/CursorUtils.java
@@ -4,15 +4,18 @@ import android.database.Cursor;
 
 import com.flyn.hastesql.core.HasteModel;
 import com.flyn.hastesql.optional.Property;
-import com.flyn.hastesql.optional.Type;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by flyn on 2014-11-11.
@@ -36,7 +39,8 @@ public class CursorUtils
 
     public static <T extends HasteModel> List<T> cursorToEntities(Class<T> clz, Cursor cursor,
                                                                   Property[] properties) throws IllegalAccessException, InvocationTargetException,
-                                                                                                InstantiationException, NoSuchMethodException
+                                                                                                InstantiationException, NoSuchMethodException,
+                                                                                                ParseException
     {
         List<T> entities = new ArrayList<T>();
         Constructor<T> constructor = clz.getDeclaredConstructor();
@@ -52,7 +56,7 @@ public class CursorUtils
             for (int i = 0; i < columnIndexArray.length; i++)
             {
                 Field field = properties[i].getField();
-                Object value = getCursorValueAt(cursor, i, properties[i].getType());
+                Object value = getCursorValueAt(cursor, i, field);
 
                 ReflectUtils.setFieldValue(field, entity, value);
             }
@@ -62,24 +66,43 @@ public class CursorUtils
         return entities;
     }
 
-    public static Object getCursorValueAt(Cursor cursor, int index, String type)
+    public static Object getCursorValueAt(Cursor cursor, int index, Field field) throws ParseException
     {
-        if (type.equals(Type.TEXT.value()))
+        Class<?> clz = field.getType();
+        if (clz.equals(String.class))
         {
             return cursor.getString(index);
-        } else if (type.equals(Type.INTEGER.value()))
+        } else if (clz.equals(CharSequence.class))
         {
-            return cursor.getInt(index);
-        } else if (type.equals(Type.BOOLEAN.value()))
+            return cursor.getString(index);
+        } else if (clz.equals(Byte.class) || clz.equals(byte.class))
+        {
+            return cursor.getString(index).getBytes()[0];
+        } else if (clz.equals(char.class) || clz.equals(Character.class))
+        {
+            return cursor.getString(index).charAt(0);
+        } else if (clz.equals(boolean.class) || clz.equals(Boolean.class))
         {
             return cursor.getInt(index) == 1;
-        } else if (type.equals(Type.DOUBLE.value()))
+        } else if (clz.equals(double.class) || clz.equals(Double.class))
         {
             return cursor.getDouble(index);
-        } else if (type.equals(Type.DATE.value()))
+        } else if (clz.equals(float.class) || clz.equals(Float.class))
         {
-            return cursor.getString(index);
-        } else if (type.equals(Type.BLOB.value()))
+            return cursor.getFloat(index);
+        } else if (clz.equals(int.class) || clz.equals(Integer.class))
+        {
+            return cursor.getInt(index);
+        } else if (clz.equals(long.class) || clz.equals(Long.class))
+        {
+            return cursor.getLong(index);
+        } else if (clz.equals(short.class) || clz.equals(Short.class))
+        {
+            return cursor.getShort(index);
+        } else if (clz.equals(Date.class))
+        {
+            return new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US).parse(cursor.getString(index));
+        } else if (clz.equals(byte[].class))
         {
             return cursor.getBlob(index);
         } else
